e7fcf6f32df9ad8ebb26657f564187df30abf434
Doug Richar <drichar@gmail.com>
2024-12-02 11:11:31 -0500

    refactor(ui): replace external Inter font with local variable fonts
    - Remove external Inter font stylesheet from index.html
    - Add local Inter variable font declarations to main.css
    - Configure Tailwind to use InterVariable as primary font

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -209,6 +209,7 @@ module.exports = {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
         algo: ['Algo', ...defaultTheme.fontFamily.sans],
       },
     },
